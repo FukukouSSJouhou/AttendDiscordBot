6111ccc3f437efb931b0fe1c73e7196d97c6b8a5
kahenteikou <kokkiemouse@gmail.com>
2022-05-18 07:09:40 +0900

    x

--- src/discordbot.py
+++ src/discordbot.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 import discord
 import settings
+TOKEN=settings.APIKEY
+client=discord.Client()
+@client.event
+async def on_Ready():
+    print("logined!")
+client.run(TOKEN)
