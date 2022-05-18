36547fe2df215997bfe2445627a205542150f1fd
kahenteikou <kokkiemouse@gmail.com>
2022-05-18 23:46:27 +0900

    a2

--- src/admins.py
+++ src/admins.py
@@ -1,6 +1,18 @@
 from discord.ext import commands
+def sqlite_register_user(userid,schoolnumber,cur2,conn2):
+    query=u'''replace into \"userinfo\"
+    (\"schoolnumber\",\"id\")
+    values (?,?)
+        '''
+    cur2.execute(query,(schoolnumber,userid))
+    conn2.commit()
 class AdminCategory(commands.Cog,name="admin"):
-    def __init__(self,bot):
+    def __init__(self,bot,cur,conn):
         super().__init__()
         self.bot=bot
-        
+        self.cur=cur
+        self.conn=conn
+    @commands.command()
+    async def registry(self,ctx, gakusekinumber):
+        sqlite_register_user(int(ctx.author.id), int(gakusekinumber),self.cur,self.conn)
+        await ctx.send("registred:" + gakusekinumber + " author: " + str(ctx.author.id))
--- src/discordbot.py
+++ src/discordbot.py
@@ -9,20 +9,9 @@ conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS \"userinfo\" (\"schoolnumber\"	INTEGER NOT NULL UNIQUE,\"id\"	INTEGER NOT NULL UNIQUE,PRIMARY KEY(\"id\"))")
 bot = commands.Bot(command_prefix='/')
-def sqlite_register_user(userid,schoolnumber):
-    query=u'''replace into \"userinfo\"
-    (\"schoolnumber\",\"id\")
-    values (?,?)
-        '''
-    cur.execute(query,(schoolnumber,userid))
-    conn.commit()
 @bot.command()
 async def attend(ctx):
     await ctx.send("attended!!")
-@bot.command()
-async def registry(ctx,gakusekinumber):
-    sqlite_register_user(int(ctx.author.id),int(gakusekinumber))
-    await ctx.send("registred:" + gakusekinumber + " author: " + str(ctx.author.id))
 @bot.command()
 async def show(ctx):
     await ctx.send("show")
