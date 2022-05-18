e3f36a70a9a2709dc83ec0e410de021776498d7d
kahenteikou <kokkiemouse@gmail.com>
2022-05-18 23:23:11 +0900

    added

--- src/discordbot.py
+++ src/discordbot.py
@@ -4,6 +4,9 @@ import settings
 import sqlite3
 from discord.ext import commands
 TOKEN=settings.APIKEY
+dbname = 'main.db'
+conn = sqlite3.connect(dbname)
+cur = conn.cursor()
 bot = commands.Bot(command_prefix='/')
 @bot.command()
 async def attend(ctx):
@@ -14,4 +17,6 @@ async def registry(ctx,gakusekinumber):
 @bot.command()
 async def show(ctx):
     await ctx.send("show")
+
 bot.run(TOKEN)
+conn.close()
