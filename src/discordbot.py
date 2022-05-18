41c59ce7f28945473b275dbce3551b9db8b85334
kahenteikou <kokkiemouse@gmail.com>
2022-05-18 23:36:08 +0900

    dbn

--- src/discordbot.py
+++ src/discordbot.py
@@ -9,11 +9,19 @@ conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS \"userinfo\" (\"schoolnumber\"	INTEGER NOT NULL UNIQUE,\"id\"	TEXT NOT NULL UNIQUE,PRIMARY KEY(\"id\"))")
 bot = commands.Bot(command_prefix='/')
+def sqlite_register_user(userid,schoolnumber):
+    query=u'''insert into \"userinfo\"
+    (\"schoolnumber\",\"id\")
+    values (?,?)
+        '''
+    cur.execute(query,(schoolnumber,userid))
+    conn.commit()
 @bot.command()
 async def attend(ctx):
     await ctx.send("attended!!")
 @bot.command()
 async def registry(ctx,gakusekinumber):
+    sqlite_register_user(str(ctx.author.id),int(gakusekinumber))
     await ctx.send("reg:" + gakusekinumber + " author: " + str(ctx.author.id))
 @bot.command()
 async def show(ctx):
