ec8ce8f40209afcc337a9a227726b26e2ff178c6
kahenteikou <kokkiemouse@gmail.com>
2022-05-19 22:57:12 +0900

    added query

--- src/scheduletool.py
+++ src/scheduletool.py
@@ -1,6 +1,13 @@
 from discord.ext import commands
 
-
+def isExistAttendCalam(schoolnumber,datekun,fiscalyear,cur2,conn2):
+    query=u'''SELECT EXISTS(SELECT * FROM Calender WHERE 
+    schoolnumber = ? AND 
+    date = ? AND 
+    fiscalyear = ?)
+    
+        '''
+    
 class ScheduleCategory(commands.Cog, name="schedule"):
     def __init__(self, bot, cur, conn,nendo):
         super().__init__()
