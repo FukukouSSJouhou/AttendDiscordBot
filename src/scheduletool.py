f0acf48a6992a07ee7f0188f58c639f30c8c756c
kahenteikou <kokkiemouse@gmail.com>
2022-05-20 07:10:23 +0900

    added2

--- src/scheduletool.py
+++ src/scheduletool.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
 import datetime
-t_delta=datetime.timedelta(hours=9)
-JST=datetime.timezone(t_delta,"JST")
-nendo=2022
-def isExistAttendCalam(schoolnumber,datekun,timekun,fiscalyear,cur2,conn2):
+
+t_delta = datetime.timedelta(hours=9)
+JST = datetime.timezone(t_delta, "JST")
+nendo = 2022
+
+
+def isExistAttendCalam(schoolnumber, datekun, timekun, fiscalyear, cur2, conn2):
     '''
     isExistAttendCalam
 
@@ -15,46 +18,57 @@ def isExistAttendCalam(schoolnumber,datekun,timekun,fiscalyear,cur2,conn2):
     :param conn2: connection?
     :return: boolkun
     '''
-    query=u'''SELECT EXISTS(SELECT * FROM Calender WHERE 
+    query = u'''SELECT EXISTS(SELECT * FROM Calender WHERE 
     schoolnumber = ? AND 
     date = ? AND 
     time = ? AND
     fiscalyear = ?)
     
         '''
-    for row in  cur2.execute(query,(schoolnumber,datekun,timekun,fiscalyear)):
+    for row in cur2.execute(query, (schoolnumber, datekun, timekun, fiscalyear)):
         for r in row:
-            if(r == 0):
+            if (r == 0):
                 return False
             else:
                 return True
-def getSchoolNumber(userid,cur2):
-    query=u'''SELECT schoolnumber FROM userinfo 
+
+
+def getSchoolNumber(userid, cur2):
+    query = u'''SELECT schoolnumber FROM userinfo 
     WHERE id = ?
     '''
     for row in cur2.execute(query, (userid,)):
         return row[0]
     return 0
+
+
 class ScheduleCategory(commands.Cog, name="schedule"):
-    def __init__(self, bot, cur, conn,nendo):
+    def __init__(self, bot, cur, conn, nendo):
         super().__init__()
         self.bot = bot
         self.cur = cur
         self.conn = conn
-        self.nendo=nendo
+        self.nendo = nendo
+
+    def sql_add_Calender(self, schoolnumber, datekun, timekun, nendo):
+        query = u'''INSERT INTO Calender (\"schoolnumber\",\"date\",\"time\",\"fiscalyear\")
+        VALUES (?,?,?,?)
+        '''
+        self.cur.execute(query, (schoolnumber, datekun, timekun, nendo))
+
     @commands.command()
-    async def attend(self,ctx):
-        idkun=int(ctx.author.id)
-        schoolnumber=getSchoolNumber(idkun,self.cur)
+    async def attend(self, ctx):
+        idkun = int(ctx.author.id)
+        schoolnumber = getSchoolNumber(idkun, self.cur)
         if schoolnumber == 0:
             await ctx.send("no registered!")
             return
-        datekun=datetime.datetime.now(JST).strftime("%Y%m%d")
-        timekun=datetime.datetime.now(JST).strftime("%H%M%S")
-        if isExistAttendCalam(schoolnumber,datekun,timekun,nendo,self.cur,self.conn) == False:
+        datekun = datetime.datetime.now(JST).strftime("%Y%m%d")
+        timekun = datetime.datetime.now(JST).strftime("%H%M%S")
+        if isExistAttendCalam(schoolnumber, datekun, timekun, nendo, self.cur, self.conn) == False:
             pass
         await ctx.send("attended!!" + str(schoolnumber))
 
     @commands.command()
-    async def show(self,ctx):
+    async def show(self, ctx):
         await ctx.send("show")
